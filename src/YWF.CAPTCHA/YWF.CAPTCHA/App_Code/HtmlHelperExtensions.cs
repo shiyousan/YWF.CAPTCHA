f1e5244516854175c884d88b006c5dffe53b2f3c
叶伟峰 <ywfuniverse@hotmail.com>
2021-01-22 17:24:34 +0800

    begin

---
+++ src/YWF.CAPTCHA/YWF.CAPTCHA/App_Code/HtmlHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace YWF.CAPTCHA.App_Code
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Captcha(this HtmlHelper helper)
+        {
+            string result = "";
+            return new MvcHtmlString(result);
+        }
+        //public static string Label(this HtmlHelper helper, string id, string title, object htmlAttributes)
+        //{
+        //    TagBuilder builder = new TagBuilder("label");
+        //    builder.GenerateId(id);
+        //    builder.MergeAttribute("title", title);
+        //    builder.MergeAttributes(new System.Web.Routing.RouteValueDictionary(htmlAttributes));
+
+        //    return builder.ToString(TagRenderMode.SelfClosing);
+        //}
+    }
+}
---
+++ src/YWF.CAPTCHA/YWF.CAPTCHA/Controllers/WidgetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YWF.CAPTCHA.Controllers
+{
+    public class WidgetController : Controller
+    {
+        public ActionResult GetCaptcha()
+        {
+            return View();
+        }
+    }
+}
