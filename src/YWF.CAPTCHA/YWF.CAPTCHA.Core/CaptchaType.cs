dbd4a4165d6331e8d8953fc3af6c0d32f9f38a25
叶伟峰 <ywfuniverse@hotmail.com>
2021-02-08 17:01:39 +0800

    验证码完善

--- src/YWF.CAPTCHA/YWF.CAPTCHA.Core/CaptchaType.cs
+++ src/YWF.CAPTCHA/YWF.CAPTCHA.Core/CaptchaType.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace YWF.CAPTCHA.Core
 {
+    public enum CaptchaType
+    {
+        //纯文本
+        PlainText,
+        //数学计算
+        Math
+    }
+
     [Flags]
-    public enum CaptchaTypeEnum
+    public enum CaptchaTextType
     {
         All=0,
         //纯数字
         Number = 1,
-        //数学计算
-        Math = 2,
         //汉字
-        ChineseCharacter = 4,
+        ChineseCharacter = 2,
         //英文大写字母
-        EnglishUpperCase = 8,
+        EnglishUpperCase = 4,
         //英文小写字母
-        EnglishLowerCase = 16
+        EnglishLowerCase = 8
     }
-    //public enum Captcha
 }
